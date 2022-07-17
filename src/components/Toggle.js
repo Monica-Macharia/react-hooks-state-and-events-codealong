d51a8ace149373729ec08373a3030dc40677d2e3
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-07-17 06:18:36 +0300

    setting a button to ON/OFF when clicked

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn]= useState(false);
+
+  function handleEvent(){
+    setIsOn((isOn) => !isOn);
+  }
+  const color = isOn ? "red" : "white";
+  return (<button style={{ background: "red" }}onClick={handleEvent}>{isOn ? "ON": "OFF"}</button>);
 }
 
 export default Toggle;
